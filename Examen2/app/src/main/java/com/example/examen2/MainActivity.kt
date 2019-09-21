4992df2600ee0aa47402f84d038cb2f46364206b
PKfede <47314404+PKfede@users.noreply.github.com>
2019-09-20 23:24:10 -0500

    hey

---
+++ Examen2/app/src/main/java/com/example/examen2/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.examen2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.R.attr.button
+import android.content.DialogInterface
+import android.view.View
+
+
+class MainActivity : AppCompatActivity() {
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
